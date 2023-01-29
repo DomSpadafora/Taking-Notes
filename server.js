12b668ca91fd297351ae111be134b7cade3f5016
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-29 12:34:09 -0500

    beging adding post code to server.js

--- server.js
+++ server.js
@@ -18,19 +18,39 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
+//Second route to notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
 //getting fetch from front end and taking json data and sending it back
 app.get('/api/notes', (req, res) => res.json(noteData));
 
-//creating post request for api/notes
+
+
+
+
+//creating post request for 'adding a new note' and sending back***activity 13/15***
 app.post('/api/notes', (req, res) => {
-  // Let the client know that their POST request was received
-  res.json(`${req.method} request received`);
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
 
-  // Show the user agent information in the terminal
-  console.info(req.rawHeaders);
+  // Prepare a response object to send back to the client
+  let response;
 
-  // Log our request to the terminal
-  console.info(`${req.method} request received`);
+  // Check if there is anything in the response body
+  if (req.body && req.body.product) {
+    response = {
+      status: 'success',
+      data: req.body,
+    };
+    res.json(`Review for ${response.data.product} has been added!`);
+  } else {
+    res.json('Request body must at least contain a product name');
+  }
+
+  // Log the response body to the console
+  console.log(req.body);
 });
 
 
